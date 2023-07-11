48d5a74d665d64474a1ff073bb0ab655f007bea9
Yuvanthika Sarathy <yuvanthika.s@tvsnext.com>
2023-07-11 11:22:58 +0530

    [Yuvanthika] Add : Computing

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("EmployeeWageComputation");
+﻿using System;
+
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+            EmployeeWage wage = new EmployeeWage();
+            wage.EmployeeAttendance();
+        }
+    }
+}
